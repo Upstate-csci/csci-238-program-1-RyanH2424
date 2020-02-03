aa190b974c97e3d845b6e4c8b85555bae6074245
RyanH2424 <60192838+RyanH2424@users.noreply.github.com>
2020-02-03 15:00:55 -0500

    Add files via upload

--- money.cpp
+++ money.cpp
@@ -1,35 +1,65 @@
 /********
- Author:
- Program: Money class - program assignment #1
- Due Date:
- Purpose:
+ Author: Ryan Hemelgarn
+ Program: Money class - program assignment #1 (Grade A version)
+ Due Date: February 3, 2020
+ Purpose: convert format of money
  ********/
 #include "money.h"
 
-// convert current currency values to a string
+// Convert currency to a string format.
 string Money::toString() {
-    return "";
+    stringstream string;
+    string << hundreds << " hundreds, " << tens << " tens, " << fives << " fives, " << ones << " ones, " << quarters << " quarters, " << dimes << " dimes, " << nickels << " nickels, " << cents << " cents";
+    return string.str();
 }
 
-// format amount as a local currency and return
+// Set account to a local currency and return.
 string Money::toCurrency(double amount) {
     stringstream ss;
 
-    ss << "$" << left << setfill('0') << setw(4) << amount;
+    ss << showbase << put_money(amount*100);
     return ss.str();
 }
 
-// convert currency to float
-// read currency values from stdin and compute value
-// return results
+// Reads the users input and gives it back to them showing what kinds of currency they have and the total.
 string Money::processChange() {
+    cin >> hundreds >> tens >> fives >> ones >> quarters >> dimes >> nickels >> cents;
+    float total2 = (float) total;
+    total = hundreds * 100 + tens * 10 + fives * 5 + ones * 1 + quarters * 0.25 + dimes * 0.10 + nickels * 0.05 + cents * 0.01;
+    // No rounding error fix needed.
+    //show all parts and then the total.
+    cout << toString() << " = $" << total;
     return "";
 }
 
-// read from stdin and process float command
-// convert float to change
-// return results
+// Take the currency given by the user and break it down into all of its parts.
 string Money::processFloat() {
+    float total2 = (float) total;
+    cin >> total2;
+    double amount;
+    amount = total2;
+    //Put the values into the variables.
+    hundreds = amount / 100;
+    amount -= hundreds * 100;
+    tens = amount / 10;
+    amount -= tens * 10;
+    fives = amount / 5;
+    amount -= fives * 5;
+    ones = amount / 1;
+    amount -= ones * 1;
+    quarters = amount / 0.25;
+    amount -= quarters * 0.25;
+    dimes = amount / 0.10;
+    amount -= dimes * 0.10;
+    nickels = amount / 0.05;
+    amount -= nickels * 0.05;
+    cents = amount / 0.01;
+    amount -= cents * 0.01;
+    //Fix for rounding error.
+    if (amount != 0)
+        cents++;
+    //Show the total and then all of the parts.
+    cout << total2 << " = " << toString();
     return "";
 }
 
@@ -37,23 +67,75 @@ string Money::processFloat() {
 // convert float to dollar words & cents
 // return results
 string Money::processCheck() {
-  return "";
+    return "";
 }
 
-// read from stdin and process change-float command
-// read float of customer payment
-// read float of transaction cost
-// calculate difference and compute change values
-// return the results
+// Take the amount someone paid and the actual cost then subtract them to get their change.
 string Money::processChangeFloat() {
-  return "";
+    float payment2 = (float) payment;
+    float total2 = (float) total;
+    cin >> payment;
+    cin >> total;
+    change = payment - total;
+    double amount;
+    amount = change;
+    //Put values into the variables
+    hundreds = amount / 100;
+    amount -= hundreds * 100;
+    tens = amount / 10;
+    amount -= tens * 10;
+    fives = amount / 5;
+    amount -= fives * 5;
+    ones = amount / 1;
+    amount -= ones * 1;
+    quarters = amount / 0.25;
+    amount -= quarters * 0.25;
+    dimes = amount / 0.10;
+    amount -= dimes * 0.10;
+    nickels = amount / 0.05;
+    amount -= nickels * 0.05;
+    cents = amount / 0.01;
+    amount -= cents * 0.01;
+    // Fix for rounding error.
+    if (amount != 0)
+        cents++;
+    // Display how much was paid and the actual cost to show the change that is owed, then break it into pieces.
+    cout << "Change back on $" << payment << " for purchase of $" << total << " is " << change << " which is " << toString();
+
+    return "";
 }
 
-// read from stdin and process change-change command
-// read change of customer payment
-// read float of transaction cost
-// calculate difference and compute change values
-// output the results
+// Take the amount someone paid and the actual cost then subtract them to get their change.
 string Money::processChangeChange() {
-  return "";
+    cin >> hundreds >> tens >> fives >> ones >> quarters >> dimes >> nickels >> cents >> total;
+    payment = hundreds * 100 + tens * 10 + fives * 5 + ones * 1 + quarters * 0.25 + dimes * 0.10 + nickels * 0.05 + cents * 0.01;
+    change = payment - total;
+    //Show the amount paid in pieces and the total cost.
+    cout << "change back on " <<  toString() << " for purchase of $" << total << " is ";
+    double amount;
+    amount = change;
+    //Add values to the variables.
+    hundreds = amount / 100;
+    amount -= hundreds * 100;
+    tens = amount / 10;
+    amount -= tens * 10;
+    fives = amount / 5;
+    amount -= fives * 5;
+    ones = amount / 1;
+    amount -= ones * 1;
+    quarters = amount / 0.25;
+    amount -= quarters * 0.25;
+    dimes = amount / 0.10;
+    amount -= dimes * 0.10;
+    nickels = amount / 0.05;
+    amount -= nickels * 0.05;
+    cents = amount / 0.01;
+    amount -= cents * 0.01;
+    //fix for rounding error.
+    if (amount != 0)
+        cents++;
+    // Show the change given from how much was paid and the total cost, then show it in pieces.
+    cout << change << " which is " << toString();
+
+    return "";
 }
--- money.h
+++ money.h
@@ -1,21 +1,22 @@
-/********
- Author:
- Program: Money class - program assignment #1
- Due Date:
- Purpose:
- ********/
-#ifndef MONEY_H
-#define MONEY_H
+//
+// Created by rheme on 1/30/2020.
+//
+
+#ifndef UNTITLED_MONEY_H
+#define UNTITLED_MONEY_H
 #include <iostream>
 #include <string>
 #include <sstream>
 #include <iomanip>
 #include <fstream>
+#include <locale>
 
 using namespace std;
 class Money
 {
 private:
+    double change;
+    double payment;
     double total;
     int hundreds, tens, fives, ones, quarters, dimes, nickels, cents;
 public:
